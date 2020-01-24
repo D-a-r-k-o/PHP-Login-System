d87429e2cca2048905d264432e78c255159eb70a
D-a-r-k-o <37869105+D-a-r-k-o@users.noreply.github.com>
2020-01-24 18:02:46 +0100

    .......

--- ajax/register.php
+++ ajax/register.php
@@ -3,20 +3,38 @@
   require_once "../inc/config.php";
 
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
-      header('Content-Type: application/json');
+      // header('Content-Type: application/json');
       $return = [];
 
+    $email = Filter::String( $_POST['email'] );
+
     // Make sure the user does not exist.
+    $findUser = $con -> prepare("SELECT user_id FROM users WHERE email = LOWER(:email) LIMIT 1");
+    $findUser -> bindParam(':email', $email, PDO::PARAM_STR);
+    $findUser -> execute();
 
-    // Make sure the user CAN be added AND is added.
+    if($findUser -> rowCount() == 1) {
+      // User exists
+      // We can also check to see if they are able to log in.
+      $return['error'] = "You already have an account";
+    } else {
+      // User does not exist, add them now.
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+      $addUser = $con -> prepare("INSERT INTO users(email, password) VALUES(LOWER(:email), :password)");
+      $addUser -> bindParam(':email', $email, PDO::PARAM_STR);
+      $addUser -> bindParam(':password', $password, PDO::PARAM_STR);
+      $addUser -> execute();
 
-    // Return the proper information back to JavaScript to redirect us.
+      $user_id = $con -> lastInsertId();
 
-    $return['redirect'] = '/dashboard.php';
+      $_SESSION['user_id'] = (int) $user_id;
+      $return['redirect'] = '/dashboard.php?message=welcome';
+    }
+
     $return['name'] = "Johnny Bravo";
 
     echo json_encode($return, JSON_PRETTY_PRINT); exit;
   } else {
-    exit('test');
+    exit('Invalid URL');
   }
 ?>
--- inc/config.php
+++ inc/config.php
@@ -2,8 +2,9 @@
     if(!defined('__CONFIG__')) {
         exit('You do not have a config file');
     }
-
+    
     include_once "classes/DB.php";
+    include_once "classes/filter.php";
 
     $con = DB::getConnection();
 ?>
