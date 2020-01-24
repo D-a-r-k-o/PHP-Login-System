3ecc6a8710c8b702b5d54ef6568398d8e1d4642f
D-a-r-k-o <37869105+D-a-r-k-o@users.noreply.github.com>
2020-01-24 20:43:40 +0100

    bla, bla, bla

---
+++ dashboard.php
@@ -0,0 +1,26 @@
+<?php 
+  define('__CONFIG__', true);
+  require_once "inc/config.php";  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="follow">
+    <title>Document</title>
+    <base href="/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css">
+</head>
+<body>
+
+<div class="uk-section uk-container">
+
+</div>
+
+<?php require_once "inc/footer.php"; ?>
+  
+</body>
+</html>
--- inc/config.php
+++ inc/config.php
@@ -2,6 +2,10 @@
     if(!defined('__CONFIG__')) {
         exit('You do not have a config file');
     }
+
+    if(!isset($_SESSION)) {
+        session_start();
+    }
     
     include_once "classes/DB.php";
     include_once "classes/filter.php";
